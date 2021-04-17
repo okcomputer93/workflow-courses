e119b21f8a6bac7a980085c4072bce3a19b5c819
Omar J <okcomputer2393@gmail.com>
2021-04-17 11:44:13 -0500

    Refactor ManageCoursesTest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function signIn(String $role = 'student', User $user = null)
+    {
+        $user = $user ?? User::factory()->create([
+                'role' => $role
+            ]);
+        $this->actingAs(
+            $user
+        );
+        return $user;
+    }
 }
