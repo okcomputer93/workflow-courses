991ef31112cef78415f80ba7c220c0bde32798ca
Omar J <okcomputer2393@gmail.com>
2021-04-20 15:29:40 -0500

    Style for header body and footer

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.app');
 });
 
 Route::post('/courses', [CoursesController::class, 'store'])->middleware('auth')->name('courses.store');
