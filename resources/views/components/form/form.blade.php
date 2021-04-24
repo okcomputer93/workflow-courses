60689bafcf19d476dc3ba7285187cdc8d50c5293
Omar J <okcomputer2393@gmail.com>
2021-04-24 11:37:28 -0500

    Form responsive design

--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -5,23 +5,25 @@
     'class' => '',
 ])
 
-<div {{ $attributes->merge(['class' => 'max-w-md h-screen sm:h-auto w-full space-y-4 bg-white p-6 sm:p-10 rounded-md shadow-md space-y-8 ' .  $class]) }}>
-    <div>
-        <img class="mx-auto h-12 w-auto" src="/images/workflow-mark.svg" alt="Workflow">
-        <h2 class="mt-6 text-center text-2xl sm:text-3xl font-extrabold text-gray-900">
-            {{ $title }}
-        </h2>
-    </div>
-    <form class="mt-8" action="{{ $action }}"
-          method="{{ ucwords($method) === 'GET' ? 'GET' : 'POST'  }}"
-    >
-        @csrf
-        @if (! in_array($method, ['GET', 'POST']))
-            @method($method)
-        @endif
+<div {{ $attributes->merge(['class' => 'max-w-md h-screen sm:h-auto w-full space-y-4 bg-white p-6 py-16 sm:p-10 rounded-md shadow-md sm:space-y-8 ' .  $class]) }}>
+   <div>
+       <div>
+           <img class="mx-auto h-12 w-auto" src="/images/workflow-mark.svg" alt="Workflow">
+           <h2 class="mt-6 text-center text-2xl sm:text-3xl font-extrabold text-gray-900">
+               {{ $title }}
+           </h2>
+       </div>
+       <form class="mt-8" action="{{ $action }}"
+             method="{{ ucwords($method) === 'GET' ? 'GET' : 'POST'  }}"
+       >
+           @csrf
+           @if (! in_array($method, ['GET', 'POST']))
+               @method($method)
+           @endif
 
-        <div class="rounded-md space-y-14 sm:space-y-8">
-           {{ $slot }}
-        </div>
-    </form>
+           <div class="rounded-md space-y-14 sm:space-y-8">
+               {{ $slot }}
+           </div>
+       </form>
+   </div>
 </div>
