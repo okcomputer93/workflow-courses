c6901388bc4b455bc8d87ed43868c5879a89b730
Omar J <okcomputer2393@gmail.com>
2021-04-27 11:24:05 -0500

    Change color on navigation bar

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -4,9 +4,9 @@
 ])
 
 @php
-    $classes = Request::routeIs($route) ? 'text-gray-900 border-indigo-500' : 'hover:text-gray-900 hover:border-indigo-500 border-transparent';
+    $classes = Request::routeIs($route) ? 'text-gray-100 border-indigo-500' : 'hover:text-gray-100 text-gray-400 hover:border-indigo-500 border-transparent';
 @endphp
 
-<a href="{{ route($route) }}" {{ $attributes->merge(['class' => 'inline-block mx-6 px-4 py-6 cursor-pointer text-base text-gray-400 font-medium border-b-2 ' . $classes . ' ' . $class]) }} >
+<a href="{{ route($route) }}" {{ $attributes->merge(['class' => 'inline-block mx-6 px-4 py-6 cursor-pointer text-base font-medium border-b-4 ' . $classes . ' ' . $class]) }} >
     {{ $slot }}
 </a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,11 @@
     <x-slot name="head">
         <title>Workflow | Home</title>
     </x-slot>
-    <x-section type="base">
-        Hello world
+    <x-section type="clean">
+        <main class="flex justify-center items-center py-10">
+            <h2 class="font-bold text-4xl">
+                <span class="text-indigo-500">Workflow</span> te ayuda a completar tu carrera
+            </h2>
+        </main>
     </x-section>
 </x-layout>
