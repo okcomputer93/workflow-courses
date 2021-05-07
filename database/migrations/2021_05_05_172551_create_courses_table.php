17a9ed951bad4927465bbba614798939b8602cf1
Omar J <okcomputer2393@gmail.com>
2021-05-07 14:08:41 -0500

    Design for single course view

--- database/migrations/2021_05_05_172551_create_courses_table.php
+++ database/migrations/2021_05_05_172551_create_courses_table.php
@@ -16,7 +16,7 @@ class CreateCoursesTable extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->unsignedInteger('rate')->default(3);
             $table->string('video_url');
             $table->string('miniature');
--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,15 +1,43 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Such an ugly view for a course</title>
-</head>
-<body>
-    <h1>{{ $course->title }}</h1>
-    <p>{{ $course->description }}</p>
-    <span>{{ $course->rate }}</span>
-</body>
-</html>
+<x-layout>
+    <x-slot name="head">
+        <title>Workflow | {{ $course->title }}</title>
+    </x-slot>
+    <x-section type="base" class="py-20">
+        <div class="py-10 bg-gradient-to-r from-indigo-500 to-category-{{ $course->category->id }} rounded-t-xl shadow-xl">
+            <div class="flex justify-between items-center">
+                <div class="flex-1 m-10">
+                    <img class="w-2/3 rounded-xl m-auto" src="{{ asset($course->miniature) }}" alt="{{ $course->title .  "'s miniature" }}">
+                </div>
+                <div class="flex-1 flex flex-col justify-center">
+                    <div class="w-2/3">
+                        <h2 class="font-bold text-3xl text-white">{{ $course->title }}</h2>
+                        <p class="mt-6 font-light text-base text-white">{{ $course->description }}</p>
+                        <div class="mt-3">
+                            <x-button href="#">Tomar este curso</x-button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="px-20 rounded-b-xl bg-category-{{ $course->category->id }}">
+           <div class="flex items-center justify-between py-4">
+               <div class="flex justify-center">
+                   <div class="flex items-center justify-center space-x-0.5">
+                       @for ($i = 1; $i <= 3; $i++)
+                           <span class="rounded block {{ $i <= $course->level->scale ? 'bg-white' : 'bg-gray-500' }}"
+                                 style="width: 7px; height: 11px"
+                           ></span>
+                       @endfor
+                       <h4 class="pl-1 text-white font-bold text-xs uppercase">{{ $course->level->name }}</h4>
+                   </div>
+                   <a href="#" class="ml-4 border border-white rounded-lg hover:text-category-{{ $course->category->id }} hover:bg-white block py-2 px-4 text-white font-bold text-xs uppercase">{{ str_replace('_', ' ', $course->category->name) }}</a>
+               </div>
+               <div class="flex justify-between items-center">
+                   <p class="text-white text-xs pr-2">Rating: </p>
+                   <x-rate :rate="$course->rate" max="5"></x-rate>
+               </div>
+           </div>
+        </div>
+    </x-section>
+</x-layout>
+
