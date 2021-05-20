7039b157fd0bf422dc97a972a71e54f7a8092272
Omar J <okcomputer2393@gmail.com>
2021-05-20 12:36:20 -0500

    Touch courses index style for empty list

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -16,7 +16,6 @@
            class="w-full appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-300 text-gray-600 rounded-md focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:z-10 sm:text-base"
            placeholder="{{ $slot }}"
            value="{{ old($name) ?? $value }}"
-           x-ref="{{ $reference }}"
            required
            @if ($focus)
                autofocus
