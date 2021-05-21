f499ede672c9cc19ca69a2e299351d62794a9a90
Omar J <okcomputer2393@gmail.com>
2021-05-21 11:37:07 -0500

    configure seeder for courses

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -13,7 +13,7 @@
             <div class="py-12 flex justify-center items-center text-center text-gray-900">
                 <div class="card bg-white p-16 shadow rounded-md">
                     <h3 class="font-bold text-3xl leading-snug max-w-md">¡Nuestros docentes están trabajando a la velocidad de la luz para ofrecerte cursos!</h3>
-                    <h4 class="mt-4 font-light text-xl text-indigo-500">Parece que han tardado un poco</h4>
+                    <h4 class="mt-4 font-light text-xl text-indigo-500">Parece que han tardado un poco...</h4>
                     <img class="mt-8 m-auto block h-96" src="/images/confused-person.svg" alt="confused person">
                 </div>
             </div>
