858433ecbb8f5d1c003d0f0d39574320bf9ca9ae
Omar J <okcomputer2393@gmail.com>
2021-05-28 12:28:27 -0500

    final touches on courses card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -7,26 +7,26 @@
     $rotation_class = "-rotate-$grades[$rotation]";
 @endphp
 
-<article class="bg-white rounded-sm shadow-lg rounded-md w-full transform hover:-translate-y-0.5 hover:shadow-xl transition-transform duration-300"
+<article class="bg-white rounded-sm shadow-lg rounded-md w-full hover:shadow-xl transition-shadow duration-300"
          style="max-width: 36rem;"
 >
     <div class="flex items-center">
-        <img class="w-44 h-44 transform {{ $rotation_class }} shadow-md"
+        <img class="w-44 h-44 transform {{ $rotation_class }} shadow-lg hover:shadow-xl transition-shadow duration-300"
              src="{{ asset($data->miniature) }}"
              alt="{{ $data->title .  "'s miniature" }}"
         >
-        <div class="flex flex-col justify-between items-start h-72 w-full p-6">
-            <div class="flex flex-wrap justify-between items-center">
-                <p class="flex-shrink-0 text-xs font-light uppercase p-1 m-1 border border-category-{{ $data->category->id }} text-category-{{ $data->category->id }} rounded-lg"
+        <div class="flex flex-col justify-between items-start h-80 w-full p-6">
+            <div class="flex flex-wrap justify-between items-center space-x-2">
+                <p class="flex-shrink-0 text-xs font-bold uppercase p-1 border border-category-{{ $data->category->id }} text-category-{{ $data->category->id }} rounded-lg"
                 >
                     {{ ucwords(str_replace('_', ' ', $data->category->name)) }}
                 </p>
-                <p class="flex-shrink-0 text-xs font-light uppercase p-1 m-1 border border-level-{{ $data->level->id }} text-level-{{ $data->level->id }} rounded-lg"
+                <p class="flex-shrink-0 text-xs font-bold uppercase p-1 border border-level-{{ $data->level->id }} text-level-{{ $data->level->id }} rounded-lg"
                 >
                     {{ ucwords(str_replace('_', ' ', $data->level->name)) }}
                 </p>
             </div>
-            <h2 class="text-2xl font-bold">{{ $data->title }}</h2>
+            <h2 class="text-3xl font-bold">{{ $data->title }}</h2>
             <div>
                 <x-rate :rate="$data->rate"
                         max="5"
@@ -36,7 +36,7 @@
             <p class="text-xs text-gray-500">{{ $data->owner->name }}</p>
             <div class="self-end">
                 <x-button href="{{ $data->path() }}"
-                          class="text-center"
+                          class="text-center text-sm px-8 rounded-full py-3 transition-colors duration-300"
                 >
                     Detalles
                 </x-button>
--- resources/views/components/skeleton-card.blade.php
+++ resources/views/components/skeleton-card.blade.php
@@ -9,19 +9,24 @@
     >
         <div class="flex items-center">
             <div class="w-44 h-44 bg-gray-200 flex-initial"></div>
-            <div class="flex-auto flex flex-col justify-between h-72 p-6 pr-8">
+            <div class="flex-auto flex flex-col justify-between h-80 p-6 pr-8">
                 <div class="flex items-center w-full h-6 space-x-2">
                     <div class="w-1/{{ $random[0] }} h-full bg-gray-200 rounded-lg"></div>
                     <div class="w-1/{{ $random[1] }} h-full bg-gray-200 rounded-lg"></div>
                 </div>
-                <div class="flex flex-col h-12 w-full space-y-2">
-                    <div class="flex items-center space-x-2 h-1/2">
-                        <div class="w-1/{{ $random[2] }} h-full bg-gray-200"></div>
+                <div class="flex flex-col h-20 w-full space-y-2">
+                    <div class="flex items-center space-x-2 h-1/3">
+                        <div class="w-1/{{ $random[0] }} h-full bg-gray-200"></div>
                         <div class="w-full h-full bg-gray-200"></div>
                     </div>
-                    <div class="flex items-center space-x-2 h-1/2">
+                    <div class="flex items-center space-x-2 h-1/3">
+                        <div class="w-full h-full bg-gray-200"></div>
+                        <div class="w-1/{{ $random[1] }} h-full bg-gray-200"></div>
+                        <div class="w-1/{{ $random[2] }} h-full bg-gray-200"></div>
+                    </div>
+                    <div class="flex items-center space-x-2 h-1/3">
+                        <div class="w-1/{{ $random[2] }} h-full bg-gray-200"></div>
                         <div class="w-1/{{ $random[3] }} h-full bg-gray-200"></div>
-                        <div class="w-1/{{ $random[4] }} h-full bg-gray-200"></div>
                     </div>
                 </div>
                 <div class="w-1/3 h-4 bg-gray-200"></div>
@@ -29,7 +34,7 @@
                     <div class="w-1/{{ $random[2] }} h-full bg-gray-200"></div>
                     <div class="w-1/{{ $random[4] }} h-full bg-gray-200"></div>
                 </div>
-                <div class="self-end w-1/4 h-10 bg-gray-200 rounded"></div>
+                <div class="self-end w-1/3 h-10 bg-gray-200 rounded px-8 rounded-full py-3"></div>
             </div>
         </div>
     </article>
