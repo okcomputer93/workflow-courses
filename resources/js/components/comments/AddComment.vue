32e77b0aea2f0ea6d62214943a0d63843031db5c
Omar J <okcomputer2393@gmail.com>
2021-06-17 16:55:19 -0500

    add vue based floating button

--- resources/js/components/comments/AddComment.vue
+++ resources/js/components/comments/AddComment.vue
@@ -39,9 +39,20 @@
                 </div>
             </form>
         </modal>
-        <floating-button position="left-bottom" v-if="canAddComments" @click="openModal">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+        <floating-button position="left-bottom"
+                         v-if="canAddComments"
+                         @click="openModal"
+        >
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 class="h-6 w-6" fill="none"
+                 viewBox="0 0 24 24"
+                 stroke="currentColor"
+            >
+                <path stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                />
             </svg>
             <span class="ml-2 font-bold text-sm">
                     Deja un comentario
