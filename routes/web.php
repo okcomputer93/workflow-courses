795d6eb09c8497bf255e574dec4ecf7ca99428e6
Omar J <okcomputer2393@gmail.com>
2021-06-22 17:24:07 -0500

    grouping routes into a resource controller

--- routes/web.php
+++ routes/web.php
@@ -27,28 +27,48 @@ Route::get('/aboutus', function () {
     return view('wip');
 })->name('aboutus');
 
+
 Route::get('/user/{any?}', [UserProfileController::class, 'index'])
     ->where('any', '(dashboard|profile)')
     ->middleware('auth')
     ->name('profile.show');
 
-Route::get('/courses', [CoursesController::class, 'index'])->name('courses.index');
-Route::post('/courses', [CoursesController::class, 'store'])->middleware('auth')->name('courses.store');
-Route::get('/courses/create', [CoursesController::class, 'create'])->middleware('auth')->name('courses.create');
-Route::get('/courses/{course:slug}/edit', [CoursesController::class, 'edit'])->middleware('auth')->name('courses.edit');
-Route::get('/courses/{course:slug}', [CoursesController::class, 'show'])->name('courses.show');
-Route::patch('/courses/{course:slug}', [CoursesController::class, 'update'])->name('courses.update');
+Route::resource('courses', CoursesController::class)
+    ->parameters(['courses' => 'course:slug'])
+    ->except('index', 'show', 'destroy')
+    ->middleware('auth');
 
-Route::post('/courses/{course:slug}/watch', [WatchCoursesController::class, 'save'])->middleware('auth')->name('courses.save');
-Route::get('/courses/{course:slug}/watch', [WatchCoursesController::class, 'watch'])->middleware('auth')->name('courses.watch');
+Route::resource('courses', CoursesController::class)
+    ->parameters(['courses' => 'course:slug'])
+    ->only('index', 'show');
 
-Route::patch('/user/role-information', [RolesController::class, 'update'])->name('user-role-information.update');
-Route::get('/user/register-professor', [RolesController::class, 'create'])->middleware('auth')->name('user-role-information.create');
+
+Route::post('/courses/{course:slug}/watch', [WatchCoursesController::class, 'save'])
+    ->middleware('auth')
+    ->name('courses.save');
+
+Route::get('/courses/{course:slug}/watch', [WatchCoursesController::class, 'watch'])
+    ->middleware('auth')
+    ->name('courses.watch');
+
+
+
+Route::patch('/user/role-information', [RolesController::class, 'update'])
+    ->name('user-role-information.update');
+
+Route::get('/user/register-professor', [RolesController::class, 'create'])
+    ->middleware('auth')
+    ->name('user-role-information.create');
+
 
 Route::middleware('only.ajax')->prefix('/api')->group(function () {
-    Route::get('/user/information', [ApiController::class, 'userInformation'])->middleware('auth');
+    Route::get('/user/information', [ApiController::class, 'userInformation'])
+        ->middleware('auth');
+
     Route::get('/courses/last', [ApiController::class, 'lastCourses']);
 
     Route::get('/courses/{course}/comments', [CommentsController::class, 'index']);
-    Route::post('/courses/{course}/comments', [CommentsController::class, 'store'])->middleware('auth');
+
+    Route::post('/courses/{course}/comments', [CommentsController::class, 'store'])
+        ->middleware('auth');
 });
