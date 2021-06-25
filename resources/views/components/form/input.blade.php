6ef3e8c723ff9889d9fa45e34b3597f17abe6bc6
Omar J <okcomputer2393@gmail.com>
2021-06-24 21:00:57 -0500

    improve input style in forms

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -9,21 +9,18 @@
     'required' => true,
 ])
 
-<div class="{{ $class }}">
-    <label for="{{ $name }}" class="sr-only">{{ $slot }}</label>
+<div class="floating-input mb-5 relative">
     <input id="{{ $name }}"
            name="{{ $name }}"
            type="{{ $type }}"
-           class="w-full appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-300 text-gray-600 rounded-md focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:z-10 sm:text-base"
+           class="w-full p-3 border border-gray-300 placeholder-gray-300 text-gray-600 rounded-md focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:z-10 sm:text-base h-14"
            placeholder="{{ $slot }}"
            value="{{ old($name) ?? $value }}"
            @if ($required)
                required
            @endif
-           @if ($focus && count($errors) === 0)
-               autofocus
-           @endif
     >
+    <label for="{{ $name }}" class="absolute top-0 left-0 px-3 py-5 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out leading-none text-gray-500" >{{ $slot }}</label>
     @error($name)
         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
     @enderror
